7a94fc1b55ee073fee272e46c6b7356321a9e5b5
arvind-ishi <avij70507@gmail.com>
2024-10-05 08:41:14 +0530

    this is the js in which topic cover

---
+++ floder_01/script.js
@@ -0,0 +1,43 @@
+//this is the some topic to cover 
+
+//async js coding 
+//function return 
+
+
+// array 
+
+//1> forEach
+//2> map
+//3> filter
+//4> find
+//5>indexOf
+
+
+
+
+
+// for Each
+
+var arr = [1,2,3,4];//creating the new array and then 
+
+
+arr.forEach(function(val){
+    console.log(val+" hello");
+
+})
+ //map 
+
+
+var secondarr = [1,2,3,4];// map is used for the array form the new array create 
+
+
+
+
+ let newarr = secondarr.map(function(e){
+    return e+23
+  //ok   //console.log(e+12);
+
+})
+
+console.log(newarr);
+
